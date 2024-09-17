c492c5d8f498b46afa0e828d30f5bd269c2b6e97
sammy wlodawski <samwlodawski@gmail.com>
2024-09-17 17:37:22 -0400

    Set blueprint for registraion/login and created utils dir for client side queries and mutayions.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,6 +1,62 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from '@apollo/client';
+// import { ADD_USER } from '../utils/mutations'
+// import { LOGIN_USER } from '../utils/mutations
+// This will be where we bring in the MUTATIONS to adduser from client side.
+
+function Signup(props) {
+  const [ formState, setFormState] = useState({ username: '', password: '', email: '', })
+  const [addUser] = useMutation(ADD_USER)
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefaultr();
+    const mutationsResponse = await addUser({
+      variables: {
+        email: formState.email,
+        username: formState.username,
+        password: formState.password
+      },
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value} = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+}
+
+function Login(props) {
+  const [ formState, setFormState] = useState({ password: '', email: '', })
+  const [loginUser] = useMutation(LOGIN_USER)
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefaultr();
+    const mutationsResponse = await loginUser({
+      variables: {
+        email: formState.email,
+        password: formState.password
+      },
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value} = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+}
+
 const Login = () => {
     return (
       <div>
+        <div><h1>Register</h1>
+        <p>User registration available soon</p></div><br />
         <h1>Login</h1>
         <p>Login page coming soon!</p>
       </div>
