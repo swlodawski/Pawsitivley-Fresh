514b91b9228d74c265db6bff6b23002182cc4372
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-09-17 18:57:19 -0500

    adding code to have graphql working

--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-// const { ApolloServer } = require('@apollo/server');
-// const { expressMiddelware } = require('@apollo/server/express4');
-// const path = require('path');
-// const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddelware } = require('@apollo/server/express4');
+const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const server = new ApolloServer({
-//     typeDefs,
-//     resolvers
-// });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
 
 
-// const startApolloServer = async () => {
-//     await server.start();
+const startApolloServer = async () => {
+    await server.start();
 
     app.use(express.urlencoded({ extended: false }));
     app.use(express.json());
 
-    // app.use('/graphql', expressMiddelware(server, {
-    //     context: authMiddleware
-    // }));
+    app.use('/graphql', expressMiddelware(server, {
+        context: authMiddleware
+    }));
 
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../client/dist')));
@@ -37,12 +37,12 @@ const app = express();
         console.log(`Example app listening at http://localhost:${PORT}`)
       );
       
-    // db.once('open', () => {
-    //     app.listen(PORT, () => {
-    //         console.log(`API Server running on ${PORT}`);
-    //         console.log(`Use graphql at http://localhost:${PORT}/graphql`);
-    //     });
-    // });
-// };
+    db.once('open', () => {
+        app.listen(PORT, () => {
+            console.log(`API Server running on ${PORT}`);
+            console.log(`Use graphql at http://localhost:${PORT}/graphql`);
+        });
+    });
+};
 
-// startApolloServer();
+startApolloServer();
