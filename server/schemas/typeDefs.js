09272e8b9a6ae89429297c8f20025e5c330a301c
sammy wlodawski <samwlodawski@gmail.com>
2024-09-17 12:02:07 -0400

    Me attempting to set up typeDefs.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const typeDefs = `
+type User {
+username: String
+password: String
+email: String
+pets: [Pet]
+orders: [Order]
+}
+type Order{
+date: Date
+user: [Users]
+
+type Pet {
+animal: String
+size: String
+allergies: [String]
+}
+type Order {
+date: Date
+products: [Product]
+users: [User]
+}
+
+type: Query {
+users: [user]
+
+}
+
+
+type: Mutations {
+}
+}
+`;
+// The typeDefs is crazy maybe we could all take a look at the queris/mutations parts. I believe I set the tables refs correctly. I could be wrong though
+
+module.exports = typeDefs;
