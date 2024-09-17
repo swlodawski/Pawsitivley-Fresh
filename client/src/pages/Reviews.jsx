04e607b5239773e0edd533a6ea89683f3b7367c3
sammy wlodawski <samwlodawski@gmail.com>
2024-09-17 13:39:49 -0400

    Added small review and about us sections to the component skeletons.

--- client/src/pages/Reviews.jsx
+++ client/src/pages/Reviews.jsx
@@ -2,7 +2,12 @@ const Reviews = () => {
   return (
     <div>
       <h1>Reviews</h1>
-      <p>reviews can go here!.</p>
+      <p>"I have been purchasing dog food from Pawsitivley Fresh for months now and I couldn't be happier with the quality and service. My dog has a sensitivity to chicken
+        and finding the right food was a challenge, but the team at this store helped me find the perfect option. Highly Recommended ~ Debra H.".</p> <br />
+      <p>"I recently switched my cat to a new brand of food from Pawsitivley Fresh and I have noticed a significant impovement in her coat and overall health.
+        The customer service was fantastic and the delivery was quick. I will definitely be a returning customer!" ~ Jim L.</p> <br />
+      <p>"I was thrilled to find Pawsitivley Fresh that offers a wide variety of dog and cat food options. The prices are competitive and the shipping
+        is fast. My pets love the food and I love that I can easily order online and have it delivered right to my doorstep. 5 stars!" ~ Erin D.</p> <br />
     </div>
   );
 };
