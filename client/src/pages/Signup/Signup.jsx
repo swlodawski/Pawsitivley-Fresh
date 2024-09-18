06d18e7874062615f3496f4cec4b14fed9ac4f86
Nevah <165732847+nevah-evans@users.noreply.github.com>
2024-09-17 20:21:35 -0500

    adding auth.js client side

--- client/src/pages/Signup/Signup.jsx
+++ client/src/pages/Signup/Signup.jsx
@@ -2,3 +2,20 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../../utils/mutations';
+
+function Signup(props) {
+    const [formState, setFormState] = ({email: '', password: ''});
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSuubmit = async (e) => {
+        e.preventDefault();
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                
+            }
+        })
+    }
+
+}
