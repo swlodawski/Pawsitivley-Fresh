347fcb7e78e24dfc5c4841371fc3ab727a97c337
sammy wlodawski <samwlodawski@gmail.com>
2024-09-18 08:41:20 -0400

    Added why section.

--- client/src/pages/Why/Why.jsx
+++ client/src/pages/Why/Why.jsx
@@ -2,7 +2,13 @@ const Why = () => {
   return (
     <div>
       <h1>Why Us</h1>
-      <p>why us?</p>
+      <p>As a dedicated pet lover, you understand the importance of providing your furry friends with the best nutrition possible.
+        That's why Postitivley Fresh is the perfect choice for all you dog and cat food needs. Our extensive selection of premium quality products ensures that you can find the 
+        perfect foods for your pets, no matter their age, size, or dietary preferences. With convenient online ordering and fast shipping,
+        you can easily stock up on all your pet's favorite foods without ever leaving the comfort of your home. Plus, our competitive prices
+        and regular discounts make it easy to save money while still providing your pets with the nutrition they deserve. Trust our store to provide
+        you with the best selection, prices and convenience for all your dog and cat food needs.
+      </p>
     </div>
   );
 };
