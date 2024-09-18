468a410e69c430c34695609bb3c5e9264e240553
sammy wlodawski <samwlodawski@gmail.com>
2024-09-18 12:54:09 -0400

    Readding changes from earlier that vanished, stupid github yeah stupid github i am calling you guys out on your own platform. Haha it seems as thought the tables have turned. Not so tough now gihub.

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -5,30 +5,6 @@ import { useMutation } from '@apollo/client';
 // import { LOGIN_USER } from '../utils/mutations
 // This will be where we bring in the MUTATIONS to adduser from client side.
 
-function Signup(props) {
-  const [ formState, setFormState] = useState({ username: '', password: '', email: '', })
-  const [addUser] = useMutation(ADD_USER)
-
-  const handleFormSubmit = async (event) => {
-    event.preventDefaultr();
-    const mutationsResponse = await addUser({
-      variables: {
-        email: formState.email,
-        username: formState.username,
-        password: formState.password
-      },
-    });
-  };
-
-  const handleChange = (event) => {
-    const { name, value} = event.target;
-    setFormState({
-      ...formState,
-      [name]: value
-    });
-  };
-}
-
 function Login(props) {
   const [ formState, setFormState] = useState({ password: '', email: '', })
   const [loginUser] = useMutation(LOGIN_USER)
@@ -50,17 +26,15 @@ function Login(props) {
       [name]: value
     });
   };
-}
 
-// const Login = () => {
-//     return (
-//       <div>
-//         <div><h1>Register</h1>
-//         <p>User registration available soon</p></div><br />
-//         <h1>Login</h1>
-//         <p>Login page coming soon!</p>
-//       </div>
-//     );
-//   };
+    return (
+      <div>
+        <div><h1>Register</h1>
+        <p>User registration available soon</p></div><br />
+        <h1>Login</h1>
+        <p>Login page coming soon!</p>
+      </div>
+    );
+  };
   
   export default Login;
