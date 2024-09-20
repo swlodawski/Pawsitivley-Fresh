544292c747175d3890d9a11fea35b14f595e0e88
sammy wlodawski <samwlodawski@gmail.com>
2024-09-19 20:27:12 -0400

    Stuff.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -30,19 +30,11 @@ type Product {
 
 type Query {
     users: [User]
-<<<<<<< HEAD
-    orders: [Order]
-=======
     orders: Order
->>>>>>> 0cb5fa3cda1dfc0fc4a03e2e9df5ec4d87066b0f
     pets: [Pet]
 }
 
-// type Mutation {
-//     addUser(username: String!, email: String, password: String!): User
-//     addOrder(userId: ID!, products: [ID!]!): Order
-//     addPet(animal: String!, size: String!, allergies: [String]): Pet
-// }
+
 `;
 
 
