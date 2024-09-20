5e912a052ffc40ec54286e87f5a4240ad5d7b0dd
SDKwapis <165666421+SDKwapis@users.noreply.github.com>
2024-09-19 20:22:11 -0400

    got server to work

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,8 @@
 // Importing Mongoose models for interacting with the db
-const User = require('../models/User');
-const Pet = require('../models/Pet');
-const Order = require('../models/Order');
-const Product = require('../models/Product');
+const User = require("../models/User");
+const Pet = require("../models/Pet");
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 
 const resolvers = {
   Query: {
@@ -18,22 +18,22 @@ const resolvers = {
 
     // Fetch all pets
     pets: async () => {
-      return await Pet.find().populate('owner'); // Returns all pets and populates the 'owner' field
+      return await Pet.find();
     },
 
     //Fetch a pet by ID
     pet: async (parent, { id }) => {
-      return await Pet.findById(id).populate('owner'); // Finds a pet by ID and populates the 'owner' field
+      return await Pet.findById(id);
     },
 
     // Fetch all orders
     orders: async () => {
-      return await Order.find().populate('user').populate('products'); // Returns all orders with populated user and products fields
+      return await Order.find().populate("user").populate("products"); // Returns all orders with populated user and products fields
     },
 
     // Fetch an order by ID
     order: async (parent, { id }) => {
-      return await Order.findById(id).populate('user').populate('products'); // Finds an order by ID and populates user and products fields
+      return await Order.findById(id).populate("user").populate("products"); // Finds an order by ID and populates user and products fields
     },
 
     //Fetch all products
@@ -49,64 +49,75 @@ const resolvers = {
 
   Mutation: {
     // Add a new user
-    addUser: async (parent, { name, email, password }) => {
-      const user = new User({ name, email, password }); // Creates a new user with name,email,and password
+    addUser: async (parent, { username, email, password }) => {
+      const user = new User({ username, email, password }); // Creates a new user with name,email,and password
       return await user.save(); // Saves the new user to the db
     },
 
     // Add a new pet
-    addPet: async (parent, { name, owner }) => {
-      const pet = new Pet({ name, owner }); // Creates a new pet instance with provided details
+    addPet: async (parent, { animal, size, allergies }) => {
+      const pet = new Pet({ animal, size, allergies }); // Creates a new pet instance with provided details
       return await pet.save(); // Saves the new pet to the db
     },
 
-
-    // Resolver to add a new order
-    addOrder: async (parent, { user, products }) => {
-      const order = new Order({ user, products }); // Creates a new order instance with provided details
-      return await order.save(); // Saves the new order to the db
+    // Add a new order
+    addOrder: async (parent, { userId, products }) => {
+      const order = new Order({ user: userId, products });
+      return await order.save();
     },
 
     // Add a new product
-    addProduct: async (parent, { name, price }) => {
-      const product = new Product({ name, price }); // Creates a new product instance with provided details
-      return await product.save(); // Saves the new product to the db
+    addProduct: async (
+      parent,
+      { name, description, image, price, ingredients, isDogFood }
+    ) => {
+      const product = new Product({
+        name,
+        description,
+        image,
+        price,
+        ingredients,
+        isDogFood,
+      });
+      return await product.save();
     },
 
     // Update an existing user by ID
-    updateUser: async (parent, { id, name, email, password }) => {
+    updateUser: async (parent, { id, username, email, password }) => {
       return await User.findByIdAndUpdate(
         id, // ID of the user to update
-        { name, email, password }, // New details to update
+        { username, email, password }, // New details to update
         { new: true, runValidators: true } // Return the updated document and validate the update
       );
     },
 
     // Update an existing pet by ID
-    updatePet: async (parent, { id, name, owner }) => {
+    updatePet: async (parent, { id, animal, size, allergies }) => {
       return await Pet.findByIdAndUpdate(
         id, // ID of the pet to update
-        { name, owner }, // New details to update
+        { animal, size, allergies }, // New details to update
         { new: true, runValidators: true } // Return the updated document and validate the update
       );
     },
 
-
     // Update an existing order by ID
-    updateOrder: async (parent, { id, user, products }) => {
+    updateOrder: async (parent, { id, userId, products }) => {
       return await Order.findByIdAndUpdate(
-        id, // ID of the order to update
-        { user, products }, // New details to update
-        { new: true, runValidators: true } // Options: return the updated document and validate the update
+        id,
+        { user: userId, products },
+        { new: true, runValidators: true }
       );
     },
 
     // Update an existing product by ID
-    updateProduct: async (parent, { id, name, price }) => {
+    updateProduct: async (
+      parent,
+      { id, name, description, image, price, ingredients, isDogFood }
+    ) => {
       return await Product.findByIdAndUpdate(
-        id, // ID of the product to update
-        { name, price }, // New details to update
-        { new: true, runValidators: true } // Options: return the updated document and validate the update
+        id,
+        { name, description, image, price, ingredients, isDogFood },
+        { new: true, runValidators: true }
       );
     },
 
@@ -134,4 +145,3 @@ const resolvers = {
 
 //Exporting resolvers to import in application
 module.exports = resolvers;
-
