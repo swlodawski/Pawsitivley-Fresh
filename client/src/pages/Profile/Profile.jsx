fe803b4e56b8a7aa5bb14c267b2f7dcc249275c7
Erin DeVine <erinj291@gmail.com>
2024-09-20 22:04:14 -0400

    added styling for the profile page

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import './Profile.css';
 
 const pets = [
-  {
-    animal: 'Dog',
-    name: 'Auggie',
-    size: 'Large',
-    allergies: ['Turkey', 'Chicken']
-  },
+
   {
     animal: 'Cat',
     name: 'Oliver',
     size: 'Small',
     allergies: ['Soy']
+  },
+  {
+    animal: 'Dog',
+    name: 'Auggie',
+    size: 'Large',
+    allergies: ['Turkey', 'Chicken']
   },
   {
     animal: 'Dog',
@@ -25,19 +26,22 @@ const pets = [
 const Profile = () => {
   return (
     <>
-    <h1 id='pets-header'>View your Pet Profiles</h1>
-    <div className='pets-container'>
-      {pets.map((pet, index) => (
-        <div key={index} className="pet-profile">
-          <h2>{pet.name}</h2>
-          <p className="animal">{pet.animal}</p>
-          <p className="size">{pet.size}</p>
-          <p className="allergies">
-            <strong>Allergies:</strong> {pet.allergies.join(', ')}
-          </p>
-        </div>
-      ))} {/* Closing the map function here */}
-    </div>
+      <h1 id='pets-header'>View your Pet Profiles</h1>
+      <div className='pets-container'>
+        {pets.map((pet, index) => (
+          <div key={index} className="pet-profile">
+            <h2>{pet.name}</h2>
+            <p className="animal">{pet.animal}</p>
+            <p className="size">{pet.size}</p>
+            <p className="allergies">
+              <strong>Allergies:</strong> {pet.allergies.join(', ')}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="button-container">
+        <button id="add-pet-btn">Add Another Pet</button>
+      </div>
     </>
   );
 };
